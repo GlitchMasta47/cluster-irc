c3d39a6ba304f23cabbc13e2fc193700e548da43
GlitchMasta47 <glitchmasta47@hotmail.com>
2018-06-28 16:02:51 -0500

    Updated loading ui + basic foundation for updat checks

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let electron = require('electron');
 let app = electron.app;
 let win = null;
+let ipc = electron.ipcMain;
 
 function createWindow () {
     // Create the browser window.
-    win = new electron.BrowserWindow({width: 800, height: 600});
+    win = new electron.BrowserWindow({width: 250, height: 300, frame: false, resizable: false, webPreferences: {devTools: false}, backgroundColor: "#2f2f2f"});
 
     // and load the index.html of the app.
     win.loadFile('the_actual_app/load.html');
@@ -26,13 +27,18 @@ app.on('window-all-closed', () => {
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit();
+    } else if (loaded == false) { // You should very much so assume that the platform is darwin, as the previous check filters all others out
+        app.quit();
     }
 });
+
+/* Once we actually get the loading ui finished, this code can be introduced.
 
 app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-        createWindow();
+    if (loaded === true) {
+        createActualWindow();
     }
 });
+*/
